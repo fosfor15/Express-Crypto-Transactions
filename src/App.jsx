ef140d74f0eb12fc790e20f0d4812e2d3ddcf04d
Dmitry Kostyuk <dima.kostyuk1508@gmail.com>
2022-04-19 15:12:47 +0300

    Build account re-connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import Button from './components/Button';
 import StatusOutput from './components/StatusOutput';
 import './styles/App.css';
@@ -10,17 +10,35 @@ function App() {
     // Подключение аккаунта
     const connectWallet = () => {
         ethereum.request({ method: 'eth_requestAccounts' })
-            .then(account => {
-                setConnectedAccounts(account[0]);
-
-                const _status = status + 'Account is connected:\n' +
-                    account[0].replace(/^(0x\w{10})\w+(\w{4})$/,
-                        (m, start, end) => `${start}.....${end}\n`);
-                setStatus(_status);
+            .then(accounts => {
+                console.log('Accounts :>> ', accounts);
+                setConnectedAccounts(accounts[0]);
             })
             .catch(error => console.log('Error :>> ', error));
     };
 
+    // Смена аккаунтов
+    useEffect(() => {
+        ethereum.on('accountsChanged', accounts => {
+            console.log('Accounts :>> ', accounts);
+            setConnectedAccounts(accounts[0]);
+        });
+    }, []);
+
+    // Вывод подключение аккаунта в статус
+    useMemo(() => {
+        if (!connectedAccount) {
+            return;
+        }
+
+        const _status = status + 'Account is connected:\n' +
+            connectedAccount.replace(/^(0x\w{10})\w+(\w{4})$/,
+                (m, start, end) => `${start}.....${end}\n`);
+        setStatus(_status);
+    }, [ connectedAccount ]);
+
+
+    // React-элемент
     return (
         <div className="App">
             <h1>Express Automation Script</h1>
